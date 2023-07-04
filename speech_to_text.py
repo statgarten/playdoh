bce436703ed3f4e603706aa3bdfed6096b1d8c6d
ycseong07 <ycseong07@phidigital.co.kr>
2023-07-04 10:42:43 +0900

    fix: 'POST /speech_to_text HTTP/1.1' 500 Internal Server Error

--- speech_to_text.py
+++ speech_to_text.py
@@ -4,19 +4,21 @@ import io
 
 def main():
     st.header("Speech-to-Text with wav2vec")
+    left_column, right_column = st.columns(2) 
 
-    uploaded_file = st.file_uploader("Choose an audio file", type=["wav", "mp3", "wma"])
+    uploaded_file = left_column.file_uploader("Choose an audio file", type=["wav", "mp3", "wma"])
+
     if uploaded_file is not None:
+        right_column.video(uploaded_file)
         wav_data = io.BytesIO(uploaded_file.read())
         if wav_data is not None:
                 response = requests.post("http://localhost:8001/speech_to_text", files={"file": wav_data})
                 if response.status_code == 200:
-                    st.success("good")
                     transcription = response.json()["transcription"]
-                    st.write("Transcription:")
-                    st.write(transcription)
+                    left_column.write("Transcription:")
+                    left_column.write(transcription[0])
                 else:
-                    st.error(response.status_code)
+                    left_column.error(response.status_code)
 
 # For running this file individually
 # if __name__ == "__main__":
