026e2d1910b208839dcd561ca47607bb6177434d
ycseong07 <ycseong07@gmail.com>
2023-08-09 16:31:34 +0900

    add: footer

--- streamlit_frontend/app.py
+++ streamlit_frontend/app.py
@@ -1,9 +1,15 @@
+import json
 import streamlit as st
 import image_classification as img_cls
 import sentiment_analysis as sent_ans
 import speech_to_text as stt
 import time_series_forecasting as timeseries
 from PIL import Image
+
+def translate(key, language):
+    with open(f'locale/main_{language}.json', "r", encoding='utf-8') as file:
+        translations = json.load(file)
+        return translations[language][key]
 
 def main():
     st.set_page_config(layout="wide")
@@ -16,14 +22,13 @@ def main():
         unsafe_allow_html=True,
     )
 
-    
     app_options = {
         "Image Classification": img_cls,
         "Sentiment Analysis": sent_ans,
         "Speech to Text": stt,
         "Time Series Forecasting": timeseries
     }
-    
+
     app_choice = st.sidebar.selectbox("", options=list(app_options.keys()))
 
     # 기본이 english
@@ -40,6 +45,30 @@ def main():
                 st.session_state.ko_en = 'ko'
 
     app_options[app_choice].main()
+
+    # hide streamlit menu and footer
+    hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden;}
+       footer {visibility: hidden;}
+       </style>
+       """
+    st.markdown(hide_default_format, unsafe_allow_html=True)
+    
+    # add customized footer
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+    st.sidebar.write("#")
+
+    text = translate('resolution', st.session_state.ko_en)
+    st.sidebar.caption(f'<div style="text-align: center;">{text}</div>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
